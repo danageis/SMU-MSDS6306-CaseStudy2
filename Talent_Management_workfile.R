d987484067c03dd7850bb0655207f19a5a8bd41b
Dana Geislinger <dg332@nau.edu>
2018-04-18 19:09:54 -0700

    Updated R workfile for #2

--- Talent_Management_workfile.R
+++ Talent_Management_workfile.R
@@ -4,13 +4,41 @@
 # for SMU MSDS 6306 Spring 2018
 #######
 
-## Clean the Raw Data
+# 2a
+# Read raw data
+dfm_attrition_data = read.csv("data/CaseStudy2-data.csv")
 
-# Read in Raw Data
-dfm_
+# Explore data structure and print number of data rows/columns
+str(dfm_attrition_data)
+cat(paste("Rows:\t",
+          dim(dfm_attrition_data)[1],
+          "\nColumns:",
+          dim(dfm_attrition_data)[2]
+          )
+    )
 
-# Clean Raw Data
-
-# Convert all Data Types to correct Types
-
-# 
+# 2b-d
+# Clean raw data names and shorten under 12 characters
+names(dfm_attrition_data)[3] = "TravelFreq"
+names(dfm_attrition_data)[6] = "Distance"
+names(dfm_attrition_data)[8] = "EducField"
+names(dfm_attrition_data)[9] = "Count"
+names(dfm_attrition_data)[10] = "EmployeeNum"
+names(dfm_attrition_data)[11] = "EnvironRate"
+names(dfm_attrition_data)[14] = "JobInvolve"
+names(dfm_attrition_data)[17] = "JobSatis"
+names(dfm_attrition_data)[18] = "Marriage"
+names(dfm_attrition_data)[19] = "MonIncome"
+names(dfm_attrition_data)[21] = "NumCompany"
+names(dfm_attrition_data)[24] = "PayIncPct"
+names(dfm_attrition_data)[25] = "Performance"
+names(dfm_attrition_data)[26] = "Relationship"
+names(dfm_attrition_data)[27] = "StdHours"
+names(dfm_attrition_data)[28] = "StockOption"
+names(dfm_attrition_data)[29] = "YearsWorked"
+names(dfm_attrition_data)[30] = "TrainLastYr"
+names(dfm_attrition_data)[31] = "WorkLifeBal"
+names(dfm_attrition_data)[32] = "YearsAtComp"
+names(dfm_attrition_data)[33] = "YearsInPos"
+names(dfm_attrition_data)[34] = "LstPromoted"
+names(dfm_attrition_data)[35] = "YearsSprvsr"
